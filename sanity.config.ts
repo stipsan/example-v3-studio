a15eb485e2173542ca86ad45bdc39993c087f9f5
Cody Olsen <stipsan@gmail.com>
2022-07-19 22:22:36 +0000

    new theme

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,8 @@
 import { createConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
+// @ts-expect-error -- TODO figure out how to get typings for this
+import { theme } from 'https://themer.creativecody.dev/api/hues?default=60929e&primary=d8172e&transparent=60929e&positive=43d675;300&caution=fbd024;200&lightest=fcfdfd&darkest=0d1415&min=1'
+
 //import {googleMapsInput} from '@sanity/google-maps-input'
 import { schemaTypes } from './schemas'
 
@@ -9,6 +12,7 @@ export default createConfig({
 
   projectId: 'dlr8fnur',
   dataset: 'production',
+  theme,
 
   plugins: [
     deskTool(),
