52c8f578d75cd708caab5ef9374994a068c1e68e
Cody Olsen <stipsan@gmail.com>
2022-07-20 00:48:34 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,9 @@
 import { createConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 
-const { theme } = await import(
-  // @ts-expect-error -- TODO figure out how to get typings for this
-  /* webpackIgnore: true */ 'https://themer.creativecody.dev/api/hues?preset=verdant&min=1'
-)
-//import {googleMapsInput} from '@sanity/google-maps-input'
+// @ts-expect-error -- TODO figure out how to get typings for this
+import { theme } from 'https://themer.creativecody.dev/api/hues?preset=rosabel&min=1'
+
 import { schemaTypes } from './schemas'
 
 export default createConfig({
@@ -14,11 +12,9 @@ export default createConfig({
 
   projectId: 'dlr8fnur',
   dataset: 'production',
-  theme,
 
   plugins: [
     deskTool(),
-    //googleMapsInput(),
   ],
 
   schema: {
