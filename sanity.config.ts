7b1ccbeb6fcadc0695a9f74f7fa0f186b725483a
Cody Olsen <stipsan@gmail.com>
2022-07-20 01:02:47 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,9 +3,15 @@ import { deskTool } from 'sanity/desk'
 
 import { schemaTypes } from './schemas'
 
+// @ts-expect-error
+const { theme } = await import(
+  /* webpackIgnore: true */ 'https://themer.creativecody.dev/api/hues?default=845b52;600&primary=f6401a&transparent=845b52;600&positive=43d675;300&caution=fbd024;200&critical=f02f34&lightest=fdfcfc&darkest=150f0d&min=1'
+)
+
 export default createConfig({
   name: 'default',
   title: 'My Sanity Project',
+  theme,
 
   projectId: 'dlr8fnur',
   dataset: 'production',
