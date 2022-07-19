bb2c0775682aff76330a9d7257be27960eb8cb31
Cody Olsen <stipsan@gmail.com>
2022-07-19 22:27:46 +0000

    async await

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,10 @@
 import { createConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
-// @ts-expect-error -- TODO figure out how to get typings for this
-import { theme } from 'https://themer.creativecody.dev/api/hues?default=60929e&primary=d8172e&transparent=60929e&positive=43d675;300&caution=fbd024;200&lightest=fcfdfd&darkest=0d1415&min=1'
 
+const { theme } = await import(
+  // @ts-expect-error -- TODO figure out how to get typings for this
+  /* webpackIgnore: true */ 'https://themer.creativecody.dev/api/hues?preset=verdant&min=1'
+)
 //import {googleMapsInput} from '@sanity/google-maps-input'
 import { schemaTypes } from './schemas'
 
