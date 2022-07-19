9dab9220727a21ea567dc72965293f190fa30c14
Cody Olsen <stipsan@gmail.com>
2022-07-20 00:43:11 +0200

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,20 @@
 import { createCliConfig } from 'sanity/cli'
+import { type UserConfig } from "vite";
 
 export default createCliConfig({
   api: {
     projectId: 'dlr8fnur',
     dataset: 'production',
+  },
+  vite: (config): UserConfig => {
+    return {
+      ...config,
+      plugins,
+      build: {
+        ...config.build,
+        minify: 'esbuild',
+        target: "esnext"
+      },
+    };
   },
 })
