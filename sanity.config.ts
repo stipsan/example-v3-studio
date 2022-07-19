f1556805c51cd84a8f04cad01a9a2d4790e058da
Cody Olsen <stipsan@gmail.com>
2022-07-20 00:56:49 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,8 @@
 import { createConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
+// @ts-expect-error -- TODO figure out how to get typings for this
+import { theme } from 'https://themer.creativecody.dev/api/hues?default=294643;600&primary=ee7975&transparent=2a4543;600&positive=43d675;300&caution=fbd024;200&lightest=fcfdfd&darkest=0d1615&min=1'
+
 
 import { schemaTypes } from './schemas'
 
@@ -9,6 +12,7 @@ export default createConfig({
 
   projectId: 'dlr8fnur',
   dataset: 'production',
+  theme,
 
   plugins: [
     deskTool(),
