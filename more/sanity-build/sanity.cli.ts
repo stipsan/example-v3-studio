09dd7b12aebf0b1371c0d3b6025bf6e4470da16d
Cody Olsen <stipsan@gmail.com>
2022-07-21 20:53:46 +0200

    Update sanity.cli.ts

--- more/sanity-build/sanity.cli.ts
+++ more/sanity-build/sanity.cli.ts
@@ -1,11 +1,12 @@
 import {createCliConfig} from 'sanity/cli'
+import type {UserConfig} from 'vite'
 
 export default createCliConfig({
   api: {
     projectId: 'kieaexhf',
     dataset: 'production',
   },
-  vite: (config) => ({
+  vite: (config: UserConfig): UserConfig => ({
     ...config,
     build: {...config.build, target: 'esnext'},
   }),
