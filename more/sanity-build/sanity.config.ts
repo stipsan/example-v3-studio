2f5cfcc9c8383cc325117dd72009dd231b48049b
Cody Olsen <stipsan@gmail.com>
2022-07-21 23:00:32 +0200

    better typescript

--- more/sanity-build/sanity.config.ts
+++ more/sanity-build/sanity.config.ts
@@ -3,10 +3,9 @@ import {deskTool} from 'sanity/desk'
 //import {googleMapsInput} from '@sanity/google-maps-input'
 import {schemaTypes} from './schemas'
 
-const {theme} = (await import(
-  // @ts-expect-error -- TODO setup themer.d.ts to get correct typings
+const {theme} = await import(
   'https://themer.creativecody.dev/api/hues?default=975e86&primary=2c6ebd&transparent=975e86&positive=43d675;300&caution=fbd024;200&lightest=fdfcfd&darkest=150d13&min=1'
-)) as {theme: import('sanity').StudioTheme}
+)
 
 export default createConfig({
   theme,
