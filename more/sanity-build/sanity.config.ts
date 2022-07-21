aac7726190da665e50cbc010c425ed072a245f4b
Cody Olsen <stipsan@gmail.com>
2022-07-21 20:28:08 +0200

    test if it builds

--- more/sanity-build/sanity.config.ts
+++ more/sanity-build/sanity.config.ts
@@ -3,7 +3,13 @@ import {deskTool} from 'sanity/desk'
 //import {googleMapsInput} from '@sanity/google-maps-input'
 import {schemaTypes} from './schemas'
 
+const {theme} = (await import(
+  // @ts-expect-error -- TODO setup themer.d.ts to get correct typings
+  'https://themer.creativecody.dev/api/hues?preset=verdant&min=1'
+)) as {theme: import('sanity').StudioTheme}
+
 export default createConfig({
+  theme,
   name: 'default',
   title: 'themer-sanity-build',
 
