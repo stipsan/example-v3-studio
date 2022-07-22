6a6bb41878009ba752d4360853b882675afa5b8e
Cody Olsen <stipsan@gmail.com>
2022-07-22 04:39:04 +0200

    setup theme loading

--- more/next-build-time/next.config.js
+++ more/next-build-time/next.config.js
@@ -4,7 +4,9 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  //
+  experimental: {
+    urlImports: ['https://themer.creativecody.dev/'],
+  },
 }
 
 module.exports = nextConfig
--- more/next-build-time/sanity.config.ts
+++ more/next-build-time/sanity.config.ts
@@ -1,9 +1,12 @@
 import {createConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
+import {theme} from 'https://themer.creativecody.dev/api/hues?preset=verdant&min=1'
 
-import {schemaTypes} from '../sanity-build/schemas'
+import {schemaTypes} from './schemas'
 
 export default createConfig({
+  theme,
+
   name: 'default',
   title: 'themer-next-build-time',
   projectId: 'kieaexhf',
