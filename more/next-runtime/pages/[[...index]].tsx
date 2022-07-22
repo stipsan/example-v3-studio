d61e6ff492040343bf533809c3f0bc3d2a4cafbb
Cody Olsen <stipsan@gmail.com>
2022-07-22 05:58:55 +0200

    moved to stipsan/example-v3-studio-next-runtime

--- more/next-runtime/pages/[[...index]].tsx
+++
@@ -1,6 +0,0 @@
-import config from '../sanity.config'
-import {Studio} from 'sanity'
-
-export default function IndexPage() {
-  return <Studio config={config} />
-}
--- more/next-runtime/pages/_app.tsx
+++
@@ -1,5 +0,0 @@
-import App from 'next/app'
-import '../style.css'
-
-
-export default App
